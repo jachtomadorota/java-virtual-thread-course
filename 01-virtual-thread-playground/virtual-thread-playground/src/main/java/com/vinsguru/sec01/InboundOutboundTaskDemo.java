997d6ee99259337675024e66b6f7a5537218317f
dorotajachtoma <jachtoma.do@gmail.com>
2026-08-21 11:05:33 +0200

    #2 -  fundamentals - creating threads using builder

--- 01-virtual-thread-playground/virtual-thread-playground/src/main/java/com/vinsguru/sec01/InboundOutboundTaskDemo.java
+++ 01-virtual-thread-playground/virtual-thread-playground/src/main/java/com/vinsguru/sec01/InboundOutboundTaskDemo.java
@@ -2,10 +2,10 @@ package com.vinsguru.sec01;
 
 public class InboundOutboundTaskDemo {
 
-    private static final int MAX_PLATFORM = 10;
+    private static final int MAX_PLATFORM = 10000;
 
     static void main() {
-        platformThreadDemo();
+        platformThreadDemo2();
 
     }
 
@@ -14,6 +14,14 @@ public class InboundOutboundTaskDemo {
             int j = i;
             Thread thread = new Thread(() -> Task.IOIntensiveTask(j));
             thread.start();
+        }
+    }
+
+    private static void platformThreadDemo2() {
+        Thread.Builder.OfPlatform thread = Thread.ofPlatform().name("dorota", 1);
+        for (int i = 0; i < MAX_PLATFORM; i++) {
+            int j = i;
+            thread.start(() -> Task.IOIntensiveTask(j));
         }
     }
 }
