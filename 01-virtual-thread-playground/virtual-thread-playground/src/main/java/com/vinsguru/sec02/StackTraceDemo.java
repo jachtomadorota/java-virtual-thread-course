98ae6f897f1ac70d60efae342b409163aec60807
dorotajachtoma <jachtoma.do@gmail.com>
2026-08-25 13:40:39 +0200

    #4 -  Virtual Threads - fundamentals 2

--- 01-virtual-thread-playground/virtual-thread-playground/src/main/java/com/vinsguru/sec02/StackTraceDemo.java
+++ 01-virtual-thread-playground/virtual-thread-playground/src/main/java/com/vinsguru/sec02/StackTraceDemo.java
@@ -1,25 +1,21 @@
 package com.vinsguru.sec02;
 
-import com.vinsguru.util.CommonUtils;
+import lombok.extern.slf4j.Slf4j;
 
-import java.time.Duration;
-
+@Slf4j
 public class StackTraceDemo {
 
-    public static void main(String[] args) {
 
-        demo(Thread.ofVirtual().name("virtual-", 1));
-
-        CommonUtils.sleep(Duration.ofSeconds(2));
+    static void main() {
+        //demo(Thread.ofPlatform());
+        demo(Thread.ofVirtual());
 
     }
 
-    private static void demo(Thread.Builder builder){
-        for (int i = 1; i <= 20 ; i++) {
+    private static void demo(Thread.Builder builder) {
+        for(int i = 0; i <= 20; i++) {
             int j = i;
-            builder.start(() -> Task.execute(j));
+            builder.start(() -> Task.executeMethod(j));
         }
     }
-
-
 }
--- 01-virtual-thread-playground/virtual-thread-playground/src/main/java/com/vinsguru/util/CommonUtils.java
+++ 01-virtual-thread-playground/virtual-thread-playground/src/main/java/com/vinsguru/util/CommonUtils.java
@@ -1,36 +1,18 @@
 package com.vinsguru.util;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.experimental.UtilityClass;
 
 import java.time.Duration;
-import java.util.stream.IntStream;
 
+@UtilityClass
 public class CommonUtils {
 
-    private static final Logger log = LoggerFactory.getLogger(CommonUtils.class);
 
-    public static void sleep(String taskName, Duration duration){
+    public static void sleep(Duration duration) {
         try {
             Thread.sleep(duration);
-        } catch (InterruptedException e) {
-            log.info("{} task is cancelled", taskName);
+        } catch (InterruptedException exception) {
+            throw new RuntimeException();
         }
     }
-
-    public static void sleep(Duration duration){
-        try {
-            Thread.sleep(duration);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static long timer(Runnable runnable){
-        var start = System.currentTimeMillis();
-        runnable.run();
-        var end = System.currentTimeMillis();
-        return (end - start);
-    }
-
 }
