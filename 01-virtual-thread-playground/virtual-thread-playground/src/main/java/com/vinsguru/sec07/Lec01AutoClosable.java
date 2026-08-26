1728e397d7dccb94dfabdd8799e1a6a7121e51ad
dorotajachtoma <jachtoma.do@gmail.com>
2026-08-26 20:56:58 +0200

    #8 - AutoClosable in ExecutorService

---
+++ 01-virtual-thread-playground/virtual-thread-playground/src/main/java/com/vinsguru/sec07/Lec01AutoClosable.java
@@ -0,0 +1,23 @@
+package com.vinsguru.sec07;
+
+import com.vinsguru.util.CommonUtils;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.Duration;
+import java.util.concurrent.Executors;
+
+@Slf4j
+public class Lec01AutoClosable {
+
+
+    static void main() {
+        Executors.newSingleThreadExecutor()
+                .submit(Lec01AutoClosable::task);
+        log.info("submitted");
+    }
+
+    private static void task() {
+        CommonUtils.sleep(Duration.ofSeconds(1));
+        log.info("task executed");
+    }
+}
