07083400c16f1972321e8e124dd43e670fcb3d33
dorotajachtoma <jachtoma.do@gmail.com>
2026-08-28 18:47:14 +0200

    #20 - using CompletableFuture in aggregator

---
+++ 01-virtual-thread-playground/virtual-thread-playground/src/main/java/com/vinsguru/sec08/aggregator/AggregatorService.java
@@ -0,0 +1,22 @@
+package com.vinsguru.sec08.aggregator;
+
+import com.vinsguru.sec07.external.Client;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+
+public class AggregatorService {
+
+    private final ExecutorService executorService;
+
+    public AggregatorService(ExecutorService executorService) {
+        this.executorService = executorService;
+    }
+
+    public Product getProduct(int id) throws ExecutionException, InterruptedException {
+        var product = CompletableFuture.supplyAsync(() -> Client.getProduct(id), executorService);
+        var rating = CompletableFuture.supplyAsync(() -> Client.getRating(id), executorService);
+        return new Product(id, product.get(), rating.get());
+    }
+}
